24ed1fba661f29b08f896d5a6d9ff8183b155de3
Joyce Zou <jzou771@gmail.com>
2024-05-03 11:57:27 -0400

    css

--- frontend/src/components/cookingpot.jsx
+++ frontend/src/components/cookingpot.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Preplist from "./preplist";
 import { Link } from "react-router-dom";
-import { getPotIngredients, removeFromPot } from "../services/datastore";
+import { addPrepItem, getPotIngredients, removeFromPot } from "../services/datastore";
 import "./cookingpot.css";
 
 const Cooking = () =>{
@@ -9,10 +9,11 @@ const Cooking = () =>{
     const [ingredients, setIngredients] = useState([]);
     const [displayPreplist, setDisplayPreplist] = useState(false);
     const url = "https://api.edamam.com/api/recipes/v2?type=public&q=";
-    const app_id = "7253ed09";
-    const app_key = "4c1d204fe079e9f56682669bbb7903bd";
+    const app_id = "7253ed09"; // put this file on env
+    const app_key = "4c1d204fe079e9f56682669bbb7903bd"; // put this file on env
     const [query, setQuery] = useState("");
     const [recomentations, setRecomentations] = useState([]);
+    const [showRec, setShowRec] = useState(false); // need this for css purposes
 
     useEffect(()=>{
         getPotIngredients((prepIngredients)=>{
@@ -34,20 +35,31 @@ const Cooking = () =>{
     }, [query]);
 
     console.log(url + query + "&app_id=" + app_id + "&app_key=" + app_key+ "&field=label&field=image&field=totalNutrients&size=5");
-    const fetchRecomentation = async() => {
-        fetch(url + query + "&app_id=" + app_id + "&app_key=" + app_key + "&field=label&field=image&field=calories&size=5")
-        .then((res)=>{
-                return res.json();
-        }).then((data)=>{
+    // referenced a website for this
+    const fetchRecomentation = async () => {
+        try {
+            const response = await fetch(url + query + "&app_id=" + app_id + "&app_key=" + app_key + "&field=label&field=image&field=calories&size=5");
+            if (!response.ok) { // if fetch is successful
+                throw new Error('Error fetching the data');
+            }
+            const data = await response.json();
             console.log("data fetched:", data);
             setRecomentations(data.hits);
-        });
-    }
+            if (data.hits.length === 0){
+                setShowRec(false);
+                alert("no such recipe found");
+            }
+            else{
+                setShowRec(true);
+            }
+            
+        } catch (error) {
+            console.error('Error fetching recommendations:', error);
+        }
+    };
+    
     console.log("recs:", recomentations)
     
-    const handleDisplay = () =>{
-        setDisplayPreplist(!displayPreplist);
-    }
     const handleEnter = () => {
         const queryItems = ingredients.map((ingredient) => ingredient.name).join(", "); // return new string by concatenating all elements in array with ","
         setQuery(queryItems);
@@ -56,40 +68,44 @@ const Cooking = () =>{
         ingredients.map((ingredient) => {
             removeFromPot(ingredient.id);
         })
+        setIngredients([]);
         setRecomentations([]);
+        setShowRec(false);
     }
-  
-        
-    return(
-        <div>
-            <Link to="/home"> back </Link>
-             <button onClick={handleDisplay} id='preplist-btn'>Prep list</button>
-                    {displayPreplist ? <div className='preplist'>
-                     <Preplist setting={boolean} />
-                    </div>: <div ></div>}
-            <div>
-                <h2>Items in the pot</h2>
-                {ingredients.map((ingredient)=>(
-                    <div key={ingredient.id}>
-                        {ingredient.name}
-                    </div>
-                ))}
-                <button onClick={handleEnter}>Enter</button>
-            </div>
+    const handleRemoveIngredients = (id,item) =>{
+        removeFromPot(id);
+        addPrepItem(id,item);
+    }
 
-            <div className="recomentations-container"> 
-                {recomentations.map((rec)=>(
-                   <div className="individual-recomendations">
-                    {rec.recipe.label}
-                    <img src={rec.recipe.image}/>
-                    <button onClick={handleCook}>Cook</button>
-                   </div>
-                ))
-            }
+    return(
+        <div className="cooking-root">
+            <Link to="/home" id="return-btn"> <img src="/assets/back.png"/></Link>
+            <div className="prep-and-cook-wrapper">
+                <div className='preplist-cooking'><Preplist setting={boolean}/></div>
+                <div className="pot-items-container">
+                    {ingredients.map((ingredient)=>(
+                        <div key={ingredient.id} className="pot-item" >
+                            <p> {ingredient.name} </p>
+                            <button onClick={()=>handleRemoveIngredients(ingredient.id, ingredient)} id="remove-ingredient-btn"><img src="/assets/x.png" id="x-img"/></button>
+                        </div>
+                    ))}
+                 <button onClick={handleEnter} id="enter-btn">Enter</button>
+                </div>
             </div>
+            <img src="/assets/preptable3.png" id='cooking-background'/>
+            {showRec ? <div className="recomentations-container"> 
+                {recomentations.map((rec)=>(
+                <div>
+                   <div className="individual-recomendations">
+                    <img src={rec.recipe.image} id="recipe-img"/>
+                    <p>{rec.recipe.label}</p> 
+                   </div>
+                   <button onClick={handleCook} id='cook-btn'>Cook</button>
+                 </div>
+                ))}
+            </div>:null}
             
          </div>
-
     )
 }
 export default Cooking;
